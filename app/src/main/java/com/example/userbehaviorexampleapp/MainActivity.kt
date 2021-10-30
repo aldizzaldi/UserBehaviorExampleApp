04b490300d2e4fdad9233c51bc66dc650fe481db
aldizzaldi <izzaldi.ifthifalli@gmail.com>
2021-10-30 17:02:05 +0700

    fixing jdk problem and first initiate firebase

--- app/src/main/java/com/example/userbehaviorexampleapp/MainActivity.kt
+++ app/src/main/java/com/example/userbehaviorexampleapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.userbehaviorexampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAnalytics = Firebase.analytics
     }
 }
